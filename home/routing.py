2e3475643bb263c00673bba1013a0ee0f42779b7
Laura Abreu <lauraabreulima05@hotmail.com>
2024-03-16 21:56:56 -0300

    config inicial

---
+++ home/routing.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
+import chat.routing
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": URLRouter(
+        chat.routing.websocket_urlpatterns
+    ),
+})
